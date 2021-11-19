a5f5b4e955e1f059f819a681ccf05a44318d2595
hayoung0402 <hy950402@gmail.com>
2021-11-20 04:25:07 +0900

    웹프로젝트 뼈대

---
+++ needs/index.js
@@ -0,0 +1,39 @@
+//기본파일인 index.js
+
+const express = require('express');
+const app = express();
+const createError = require('http-errors');
+const path = require('path')
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+module.exports = app;
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+app.listen(5000, function(){
+    console.log('Listening at 5000');
+});
